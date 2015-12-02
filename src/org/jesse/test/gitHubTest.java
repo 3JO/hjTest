e979234b4f7828683df282c0a4a5bd2be340a2e9
hjyoon1120 <hjyoon1120@gmail.com>
2015-12-02 17:39:37 +0900

    Test to share project with git-hub

---
+++ src/org/jesse/test/gitHubTest.java
@@ -0,0 +1,11 @@
+package org.jesse.test;
+
+public class gitHubTest {
+	
+	public static void main(String[] args) {
+		System.out.println("BitJava#75");		
+		System.out.println("Share my project with git-hub !!!");
+		
+	}
+
+}
